64014835d8849804aa83fde70c46e4d08fb7e8e2
textfox233 <78984659+textfox233@users.noreply.github.com>
2023-05-26 15:55:45 +0100

    Player Follow Experiments
    Attempting to clean up the player follow functionality in the behaviour tree. gonna get a little messy probably

--- Source/SirDingus/SirDingusAIController.cpp
+++ Source/SirDingus/SirDingusAIController.cpp
@@ -2,7 +2,9 @@
 
 
 #include "SirDingusAIController.h"
+
 #include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 void ASirDingusAIController::BeginPlay()
 {
@@ -14,8 +16,15 @@ void ASirDingusAIController::BeginPlay()
 	if (AIBehavior)
 	{
 		RunBehaviorTree(AIBehavior);
+		
+		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
+		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+		//GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsInt(TEXT("Key"), 1);
 	}
+
+	// Debug Msg
 	else if (GEngine)
 	{
 		GEngine->AddOnScreenDebugMessage(
@@ -31,23 +40,33 @@ void ASirDingusAIController::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 	
-	//APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	////APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
 	////Alive?
 	////Yes
 	////	See Player (In Range)?
-	//if(LineOfSightTo(PlayerPawn))
+	////if(LineOfSightTo(PlayerPawn))
 	////	Yes
-	//{
+	////{
 	//	//	Run At Player
-	//	MoveToActor(PlayerPawn, 5.f);
+	//	//MoveToActor(PlayerPawn, 5.f);
 	//	//	Attack
-	//}
+
+	//	// Observe Player Position
+	//	//GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+	//	//GetBlackboardComponent()->SetValueAsObject(TEXT("PlayerActor"), PlayerPawn);
+	//	//GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
+
+	////}
 	////	No
 	//else
 	//{
-	//	StopMovement();
+	//	//StopMovement();
 	//	//	Return To Original
+
+	//	// cannot see player
+	//	//GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+	//	//GetBlackboardComponent()->ClearValue(TEXT("PlayerActor"));
 	//}
 	////	No
 	////	Do Nothing
