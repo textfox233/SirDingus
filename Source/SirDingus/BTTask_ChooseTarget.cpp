cb14547c4ea8f54f5ad9548fd02c6a941ac99f74
textfox233 <78984659+textfox233@users.noreply.github.com>
2023-06-11 18:08:25 +0100

    Got Access to Individual PlayerStates
    Able to access individual PlayerStates via iterator. Need to try using this to assign players randomly first, will have to think more later about what logic I actually want the characters to use when picking.

--- Source/SirDingus/BTTask_ChooseTarget.cpp
+++ Source/SirDingus/BTTask_ChooseTarget.cpp
@@ -2,6 +2,7 @@
 
 
 #include "BTTask_ChooseTarget.h"
+#include "GameFramework/GameState.h"
 #include "AIController.h"
 
 EBTNodeResult::Type UBTTask_ChooseTarget::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
@@ -11,12 +12,21 @@ EBTNodeResult::Type UBTTask_ChooseTarget::ExecuteTask(UBehaviorTreeComponent& Ow
 	APawn* TargetPawn;
 	
 	if (GetWorld()->GetNumPlayerControllers() > 1)
+	// more than 1 player
 	{
 		TargetPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
 		OwnerComp.GetAIOwner()->SetFocus(TargetPawn);
+
+		FConstPlayerControllerIterator i = GetWorld()->GetPlayerControllerIterator();
+
+		UE_LOG(LogTemp, Warning, TEXT("Player [%d]: %s"), i.GetIndex(), *i->Get()->GetName());
+
+		i++;
+
+		UE_LOG(LogTemp, Warning, TEXT("Player [%d]: %s"), i.GetIndex(), *i->Get()->GetName());
 	}
 	else
-	// simple, pick player 1
+	// only 1 player, pick that one
 	{
 		TargetPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
 		OwnerComp.GetAIOwner()->SetFocus(TargetPawn);
@@ -24,6 +34,15 @@ EBTNodeResult::Type UBTTask_ChooseTarget::ExecuteTask(UBehaviorTreeComponent& Ow
 
 	UE_LOG(LogTemp, Warning, TEXT("set %s focus to %s"), *OwnerComp.GetAIOwner()->GetName(), *TargetPawn->GetName());
 	UE_LOG(LogTemp, Warning, TEXT("Number of player controllers is %d"), GetWorld()->GetNumPlayerControllers());
+
+	//GetWorld()->GetGameState()->PlayerArray;
+
+	//for (FConstPlayerControllerIterator i = GetWorld()->GetPlayerControllerIterator(); i; i++)
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("Player [%d]: %s"), i.GetIndex(), *i->Get()->GetName());
+	//}
+	
+
 
 	return EBTNodeResult::Succeeded;
 }
