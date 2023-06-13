09c8efebc32045b6148e09c8f5351d32ae0bf479
textfox233 <78984659+textfox233@users.noreply.github.com>
2023-06-13 17:19:21 +0100

    New C++ Player Class
    Reparented the player character to a new C++ class named and fixed all the immediate blueprint errors that came up. Will have to watch if anymore come up as I go.

---
+++ Source/SirDingus/KnightCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SirDingusCharacter.h"
+#include "KnightCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIRDINGUS_API AKnightCharacter : public ASirDingusCharacter
+{
+	GENERATED_BODY()
+	
+};
